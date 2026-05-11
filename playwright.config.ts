47bc5230aa7a8faba2f420348b72369cf7ebbb0b
torsakun <towsakun@gmail.com>
2026-05-11 15:36:28 +0700

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  expect: {
+    timeout: 5000
+  },
+  fullyParallel: true,
+  retries: 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['list'],
+    ['./qase-reporter.js'] // นี่คือตัว Custom Reporter ของเรา
+  ],
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+  },
+});
